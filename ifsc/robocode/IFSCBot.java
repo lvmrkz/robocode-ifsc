87532001b12cf09334cfc640434ffe6bbcc7b6e6
cauajpeg <cauamarquescabral11@gmail.com>
2026-05-24 19:47:05 -0300

    Contador de kill e boss mode

--- ifsc/robocode/IFSCBot.java
+++ ifsc/robocode/IFSCBot.java
@@ -4,69 +4,111 @@ import java.awt.Color;
 
 // IFSCBot - a robot by (Luan, Ana B, Cauã, Sarah)
 
-public class IFSCBot extends Robot
-{
-	
-	public void run() {
-		
-		setColors(Color.red,Color.blue,Color.green);
+public class IFSCBot extends Robot {
 
-		while(true) {
-			ahead(150);
-			turnRight(90);
+    
+    int kills = 0;
+    boolean bossMode = false;
 
-			turnGunRight(360);
+    public void run() {
 
-			back(100);
-			turnLeft(90);
-		}
-	}
+        setColors(Color.white, Color.yellow, Color.yellow);
 
-	public void onScannedRobot(ScannedRobotEvent e) {
+        while(true) {
 
-    turnGunRight(e.getBearing());
+            ahead(150);
 
-    double distancia = e.getDistance();
+            turnRight(90);
 
-    if (getGunHeat() == 0) {
+            turnGunRight(360);
 
-        if (distancia < 100) {
-            fire(3);
+            back(100);
 
-        } else if (distancia < 300) {
-            fire(2);
+            turnLeft(90);
 
-        } else {
-            fire(1);
+            
+            if(bossMode) {
+
+                turnGunRight(720);
+
+                ahead(250);
+            }
         }
     }
 
+    public void onScannedRobot(ScannedRobotEvent e) {
+
+        turnGunRight(e.getBearing());
+
+        double distancia = e.getDistance();
+
+        if (getGunHeat() == 0) {
+
+            if (distancia < 100) {
+
+                fire(bossMode ? 3 : 2);
+
+            } else if (distancia < 300) {
+
+                fire(bossMode ? 2.5 : 1.5);
+
+            } else {
+
+                fire(1);
+            }
+        }
+
+        
+        if (Math.random() > 0.5) {
+
+            turnRight(30);
+
+        } else {
+
+            turnLeft(30);
+        }
+
+        ahead(50);
+    }
+
+    public void onHitByBullet(HitByBulletEvent e) {
+
+        turnRight(45);
+
+        back(20);
+    }
+
+    public void onHitWall(HitWallEvent e) {
+
+        back(100);
+
+        if (e.getBearing() > 0) {
+
+            turnLeft(90);
+
+        } else {
+
+            turnRight(90);
+        }
+
+        ahead(120);
+    }
+
     
-    if (Math.random() > 0.5) {
-        turnRight(30);
-    } else {
-        turnLeft(30);
+    public void onRobotDeath(RobotDeathEvent e) {
+
+        kills++;
+
+        out.println("Kills: " + kills);
+
+       
+        if(kills >= 3 && !bossMode) {
+
+            bossMode = true;
+
+            setColors(Color.black, Color.red, Color.red);
+
+            out.println("=== BOSS MODE ATIVADO ===");
+        }
     }
-
-    ahead(50);
-}
-	public void onHitByBullet(HitByBulletEvent e) {
-		
-         turnRight(45);
-         back(20);
-    }
-	
-	public void onHitWall(HitWallEvent e) {
-		
-		 back(100);
-
-    if (e.getBearing() > 0) {
-        turnLeft(90);
-    } else {
-        turnRight(90);
-    }
-
-    ahead(120);
-
-	}	
 }
